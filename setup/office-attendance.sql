644ac7bed8d5607f637d2fe2417d053f44361bc6
Vivek Singh <petmongrels@gmail.com>
2023-07-25 12:05:20 +0530

    office attendance table and data setup

---
+++ setup/office-attendance.sql
@@ -0,0 +1,30 @@
+drop table if exists attendance;
+create table attendance (id serial primary key, employee_id int, record_date date, present bool);
+insert into attendance (employee_id, record_date, present) values
+                        (1, '2023-01-01', true),
+                        (1, '2023-01-02', true),
+                        (1, '2023-01-03', false),
+                        (1, '2023-01-04', true),
+                        (1, '2023-01-05', true),
+                        (1, '2023-01-06', true);
+insert into attendance (employee_id, record_date, present) values
+                        (2, '2023-01-01', false),
+                        (2, '2023-01-02', true),
+                        (2, '2023-01-03', true),
+                        (2, '2023-01-04', true),
+                        (2, '2023-01-05', true),
+                        (2, '2023-01-06', true);
+insert into attendance (employee_id, record_date, present) values
+                        (3, '2023-01-01', true),
+                        (3, '2023-01-02', true),
+                        (3, '2023-01-03', true),
+                        (3, '2023-01-04', true),
+                        (3, '2023-01-05', true),
+                        (3, '2023-01-06', false);
+insert into attendance (employee_id, record_date, present) values
+                        (4, '2023-01-01', true),
+                        (4, '2023-01-02', true),
+                        (4, '2023-01-03', true),
+                        (4, '2023-01-04', true),
+                        (4, '2023-01-05', true),
+                        (4, '2023-01-06', false);
